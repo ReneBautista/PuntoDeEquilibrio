0acfd9df1b343acd2a980338a1ef7e912f25edf6
Rene Bautista <renebautista@gmail.com>
2021-07-15 10:12:11 -0600

    Imprime el resultado

--- PuntoEquilibrio.py
+++ PuntoEquilibrio.py
@@ -1,8 +1,10 @@
 print("Soy un programa de Python")
 
-Cf = float(input("Ingrese el costo variable (Cf)"))
-Cv = float(input("Ingrese el costo variable (Cv)"))
-Pv = float(input("Ingrese el costo variable (Pv)"))
+Cf = float(input("Ingrese el costo variable (Cf): "))
+Cv = float(input("Ingrese el costo variable (Cv): "))
+Pv = float(input("Ingrese el costo variable (Pv): "))
 
 resultado = Cf + Cv + Pv
 
+print("El resultado es:")
+print(resultado)
